62fb87a4614e4cd222c217f0fcb1f7cd1e9a8673
Eugene Leonov <yauheni.liavonau@fabros-team.com>
2022-08-17 11:21:04 +0300

    Add login bg image, update styles

--- src/domains/login/PageLogin/PageLogin.js
+++ src/domains/login/PageLogin/PageLogin.js
@@ -8,7 +8,13 @@ const PageLogin = () => {
   return (
     <Layout className="login">
       <Row>
-        <Card style={{ textAlign: 'center', boxShadow: 'rgb(34 60 80 / 5%) 0px 0px 8px 2px' }}>
+        <Card
+          style={{
+            textAlign: 'center',
+            boxShadow: 'rgb(34 60 80 / 5%) 0px 0px 8px 2px',
+            background: '#272727',
+          }}
+        >
           <h1>Login</h1>
           <p className="da-mt-sm-0 da-mt-8 da-text-color-black-60">
             Welcome back, please login to your account.
