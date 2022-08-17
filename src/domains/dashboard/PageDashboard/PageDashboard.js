b2c7838a35a84e5bd7c1244fd7b388283536fd09
Eugene Leonov <yauheni.liavonau@fabros-team.com>
2022-08-17 10:56:31 +0300

    Update styles, delete page content

--- src/domains/dashboard/PageDashboard/PageDashboard.js
+++ src/domains/dashboard/PageDashboard/PageDashboard.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Layout } from 'antd';
 
 
-
-const { Content } = Layout;
 
 const PageDashboard = () => {
   return (
     <div>
-      <Content>
-        Dashboard page
-      </Content>
+      Dashboard page
     </div>
   );
 };
